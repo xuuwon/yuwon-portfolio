d9a162dc07a9cbaa952bd7c608daa94d6017aed8
xuuwon <dumpling1011@gmail.com>
2025-04-04 17:09:02 +0900

    refactor: Main 컴포넌트에 시맨틱 태그 적용

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,7 +2,8 @@ import { ScrollAnimation } from "@lasbe/react-scroll-animation";
 
 const Main = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-background">
+    <section className="flex items-center justify-center min-h-screen bg-background">
+      {/* 주요 블록 */}
       <ScrollAnimation
         startingPoint="bottom"
         duration={2}
@@ -10,21 +11,27 @@ const Main = () => {
         delay={0.5}
         repeat
       >
-        <div className="w-[1150px] h-[570px] bg-content shadow-custom p-9 flex flex-col justify-between">
-          <div className="flex flex-col w-[850px] gap-3">
-            <p className="text-[120px] self-start leading-none font-main">
+        <article className="w-[1150px] h-[570px] bg-content shadow-custom p-9 flex flex-col justify-between">
+          {/* 독립적인 콘텐츠 영역 */}
+          <header className="flex flex-col w-[850px] gap-3">
+            {/* 제목 영역 */}
+            <h1 className="text-[120px] self-start leading-none font-main">
               FRONT-END DEVELOPER
+            </h1>
+            <hr className="w-[855px] h-0.5 bg-background" />
+            <h2 className="self-end text-xl">YUWON PORTFOLIO</h2>
+          </header>
+
+          <footer className="self-end text-xl text-right">
+            {/* 맨 아래 정보 영역 */}
+            <p>
+              사용자의 입장에서 늘 생각하며
+              <br /> 배움과 성장을 즐기는 <br /> 프론트엔드 개발자 조유원입니다.
             </p>
-            <div className="w-[855px] h-0.5 bg-background "></div>
-            <p className="self-end text-xl">YUWON PORTFOLIO</p>
-          </div>
-          <p className="self-end text-xl text-right">
-            사용자의 입장에서 늘 생각하며
-            <br /> 배움과 성장을 즐기는 <br /> 프론트엔드 개발자 조유원입니다.
-          </p>
-        </div>
+          </footer>
+        </article>
       </ScrollAnimation>
-    </div>
+    </section>
   );
 };
 
