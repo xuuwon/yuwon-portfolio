b5c4cd91abe0ba9ce35b6385b54d6b98fcdb9967
xuuwon <dumpling1011@gmail.com>
2025-04-10 22:54:21 +0900

    feat: 프로젝트 카드 렌더링 추가

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,18 +1,34 @@
+import { projectData } from "./data/projectData";
 import ProjectCard from "./ProjectCard";
+import { motion } from "framer-motion";
 
 const Projects = () => {
   return (
-    <div>
-      <ProjectCard
-        link="/public/jeju.jpg"
-        team="팀"
-        type="프론트엔드 개발"
-        title="도르멍
-- 제주의 하루를 선물해 드립니다."
-        intro="제주도에서 새로움을 느끼고 싶은
-여행자들을 위한 랜덤 여행 서비스입니다."
-      />
-    </div>
+    <motion.div
+      initial={{ opacity: 0, y: 50 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: false, amount: 0.2 }}
+      transition={{
+        ease: "easeInOut",
+        duration: 2,
+        delay: 0.1,
+      }}
+    >
+      <section className="w-[1150px] mx-auto flex justify-end py-16 overflow-y-scroll h-screen scrollbar-hide">
+        <section className="w-[870px] text-lg text-background flex flex-wrap gap-7">
+          {projectData.map((project) => {
+            return (
+              <ProjectCard
+                team={project.team}
+                type={project.type}
+                title={project.title}
+                intro={project.intro}
+              />
+            );
+          })}
+        </section>
+      </section>
+    </motion.div>
   );
 };
 
