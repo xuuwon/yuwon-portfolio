a63d3fd576aae173cc3dbb2e855f619f23815b56
xuuwon <dumpling1011@gmail.com>
2025-04-10 22:52:56 +0900

    fix: ProjectCardProps 타입 수정 및 데이터 수정

--- src/components/type/type.ts
+++ src/components/type/type.ts
@@ -1,5 +1,5 @@
 type ProjectCardProps = {
-  link: string; // 이미지 링크 (예: "/jeju.jpg")
+  // link: string; // 이미지 링크 (예: "/jeju.jpg")
   team: string; // 팀 이름 또는 태그 (예: "팀")
   type: string; // 프로젝트 유형 (예: "프론트엔드 개발")
   title: string; // 프로젝트 제목 (예: "도르멍 - 제주의 하루를 선물해 드립니다.")
