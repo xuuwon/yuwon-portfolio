4f388693f54e2aae9a33fff17f110ce4f1883f97
xuuwon <dumpling1011@gmail.com>
2025-04-13 19:48:17 +0900

    feat: 라우팅 구조 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollRouter from "./components/ScrollRouter";
+import ProjectDetail from "./components/ProjectDetail";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/*" element={<ScrollRouter />} />
+        <Route path="/projects/:projectId" element={<ProjectDetail />} />
+        <Route path="/" element={<ScrollRouter />} />
+        <Route path="/aboutMe" element={<ScrollRouter />} />
+        <Route path="/project" element={<ScrollRouter />} />
+        <Route path="/experience" element={<ScrollRouter />} />
+        <Route path="/contact" element={<ScrollRouter />} />
       </Routes>
     </BrowserRouter>
   );
