4389baad7bd3edbc9dc17131a08389ba45b7f861
xuuwon <dumpling1011@gmail.com>
2025-04-14 17:01:11 +0900

    feat: 애니메이션 추가

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,23 +1,35 @@
 import { experienceData } from "./data/experienceData";
 import ExperienceCard from "./ExperienceCard";
+import { motion } from "framer-motion";
 
 const Experience = () => {
   return (
-    <section className="w-[1150px] mx-auto flex justify-end py-16 h-screen overflow-y-scroll scrollbar-hide">
-      <section className="w-[870px] text-lg flex flex-col gap-7">
-        {experienceData.map((data) => {
-          return (
-            <ExperienceCard
-              title={data.title}
-              type={data.type}
-              period={data.period}
-              intro={data.intro}
-              image={data.image}
-            />
-          );
-        })}
+    <motion.div
+      initial={{ opacity: 0, y: 50 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: false, amount: 0.2 }}
+      transition={{
+        ease: "easeInOut",
+        duration: 2,
+        delay: 0.1,
+      }}
+    >
+      <section className="w-[1150px] mx-auto flex justify-end py-16 h-screen overflow-y-scroll scrollbar-hide">
+        <section className="w-[870px] text-lg flex flex-col gap-7">
+          {experienceData.map((data) => {
+            return (
+              <ExperienceCard
+                title={data.title}
+                type={data.type}
+                period={data.period}
+                intro={data.intro}
+                image={data.image}
+              />
+            );
+          })}
+        </section>
       </section>
-    </section>
+    </motion.div>
   );
 };
 
