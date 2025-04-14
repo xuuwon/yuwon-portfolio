a1aefd8e33f863c6fee54532fe2206cb658a9af3
xuuwon <dumpling1011@gmail.com>
2025-04-14 16:48:51 +0900

    fix: 이미지 경로 수정

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -15,7 +15,7 @@ const AboutMe = () => {
       <section className="w-[1150px] mx-auto flex justify-end py-16">
         <section className="w-[870px] text-lg text-content flex flex-col gap-4">
           <section className="flex gap-3">
-            <img src="/public/photo.jpg" className="w-[150px] rounded-3xl" />
+            <img src="/photo.jpg" className="w-[150px] rounded-3xl" />
             <div className="flex flex-col gap-3 p-3">
               <div className="flex gap-3">
                 <p className="text-2xl">조유원</p>
