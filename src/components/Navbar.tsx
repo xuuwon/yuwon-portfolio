afa853ca1fd2da63c0bcaca3ae5eb22e05b28047
xuuwon <dumpling1011@gmail.com>
2025-05-10 19:40:57 +0900

    showNav props 추가 및 트랜지션 추가

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,57 +1,42 @@
-import { Link } from "react-router-dom";
-import { ScrollAnimation } from "@lasbe/react-scroll-animation";
+import { SectionId } from "./ScrollRouter";
 
-const Navbar = () => {
+const Navbar = ({
+  scrollToSection,
+  showNav,
+}: {
+  scrollToSection: (id: SectionId) => void;
+  showNav: boolean;
+}) => {
   const underlineAnimation = `after:absolute after:left-0 after:bottom-0 after:h-[2px] after:w-0 after:bg-content after:transition-all after:duration-300 hover:after:w-full`;
 
   return (
-    <section className="flex justify-center">
-      <ScrollAnimation startingPoint="bottom" duration={1} amount="sm">
-        <nav className="fixed w-[1150px] top-[100px] z-50 flex flex-col items-start gap-6 text-3xl text-content">
-          <Link to="/aboutMe" className="relative">
-            <span
-              className={`
-                ${underlineAnimation}
-                ${
-                  location.pathname === "/aboutMe"
-                    ? "after:w-full"
-                    : "after:w-0 hover:after:w-full"
-                }
-              `}
-            >
-              About Me
-            </span>
-          </Link>
-          <Link to="/project" className="relative">
-            <span
-              className={`
-                ${underlineAnimation}
-                ${
-                  location.pathname === "/project"
-                    ? "after:w-full"
-                    : "after:w-0 hover:after:w-full"
-                }
-              `}
-            >
-              Projects
-            </span>
-          </Link>
-          <Link to="/experience" className="relative">
-            <span
-              className={`
-                ${underlineAnimation}
-                ${
-                  location.pathname === "/experience"
-                    ? "after:w-full"
-                    : "after:w-0 hover:after:w-full"
-                }
-              `}
-            >
-              Experience
-            </span>
-          </Link>
-        </nav>
-      </ScrollAnimation>
+    <section
+      className={`flex justify-center transition-opacity duration-500 ease-in-out ${
+        showNav
+          ? "opacity-100 pointer-events-auto"
+          : "opacity-0 pointer-events-none"
+      }`}
+    >
+      <nav className="fixed w-[1150px] top-[100px] z-50 flex flex-col items-start gap-6 text-3xl text-content">
+        <button
+          onClick={() => scrollToSection("aboutMe")}
+          className={`relative ${underlineAnimation}`}
+        >
+          About Me
+        </button>
+        <button
+          onClick={() => scrollToSection("project")}
+          className={`relative ${underlineAnimation}`}
+        >
+          Projects
+        </button>
+        <button
+          onClick={() => scrollToSection("experience")}
+          className={`relative ${underlineAnimation}`}
+        >
+          Experience
+        </button>
+      </nav>
     </section>
   );
 };
