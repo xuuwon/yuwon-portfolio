dd39b36c0f90074eec17e1a8ff51baaafba6e456
xuuwon <dumpling1011@gmail.com>
2025-05-12 21:50:06 +0900

    style: 높이 등 스타일 수정

--- src/components/ExperienceCard.tsx
+++ src/components/ExperienceCard.tsx
@@ -8,27 +8,31 @@ const ExperienceCard = ({
   image,
 }: ExperienceCardProps) => {
   return (
-    <section
-      className="w-[828px] min-h-[275px] border-2 text-content border-content rounded-3xl flex overflow-hidden text-base"
-      style={{ height: "275px" }}
-    >
+    <section className="w-[828px] border-2 text-content border-content rounded-3xl flex overflow-hidden text-base">
+      {/* 텍스트 영역 */}
       <section className="w-[528px] p-7 flex flex-col gap-3">
         <p className="text-xl">{title}</p>
-        <div className="flex gap-2">
-          {type.map((type) => {
-            return (
-              <span className="px-2 py-1 text-sm rounded-full bg-content text-background">
-                {type}
-              </span>
-            );
-          })}
+        <div className="flex flex-wrap gap-2">
+          {type.map((type, index) => (
+            <span
+              key={index}
+              className="px-2 py-1 text-sm rounded-full bg-content text-background"
+            >
+              {type}
+            </span>
+          ))}
         </div>
         <p>{period}</p>
-        <p>{intro}</p>
+        <p className="leading-7 whitespace-pre-line">{intro}</p>
       </section>
 
-      <section className="w-[300px]">
-        <img src={image} className="h-full w-[300px] object-cover" />
+      {/* 이미지 영역 */}
+      <section className="w-[300px] h-auto">
+        <img
+          src={image}
+          className="object-cover w-full h-full"
+          style={{ maxHeight: "100%" }}
+        />
       </section>
     </section>
   );
